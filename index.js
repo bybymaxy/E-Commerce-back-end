3c1cbe185b0c67d2600ea3f260e4b7c23a0e4d4c
bybymaxy <141274790+bybymaxy@users.noreply.github.com>
2023-12-27 17:02:22 -0800

    updates to sequelize in index.js

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ const tagRoutes = require('./routes/tagRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const { Sequelize, DataTypes } = require('sequelize');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+  }
+);
+
+module.exports = sequelize;
+
 // Use the routes
 app.use('/api/categories', categoryRoutes);
 app.use('/api/products', productRoutes);
